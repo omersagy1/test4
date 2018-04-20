f43aa5af1a7bded4450b018a482bb74018ade2b1
omersagy <omersagyemails@gmail.com>
2018-04-20 00:46:06 -0400

    proper functional components with argument names in props

--- src/index.js
+++ src/index.js
@@ -1,31 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from 'material-ui/Button';
 
-const NUM_ROWS = 3;
-const NUM_COLS = 10;
 
-function Cell() {
+const Cell = () => {
   return <span>O</span>;
 }
 
-function Row(num_cells) {
+
+const Row = ({num_cells}) => {
   let cells = [];
   for (let i = 0; i < num_cells; i++) {
-    cells.push(Cell());
+    cells.push(<Cell/>);
   }
   return <div>{cells}</div>
 }
 
-function Grid(num_rows, num_columns) {
+
+const Grid = ({num_rows, num_columns}) => {
   let rows = [];
   for (let i = 0; i < num_rows; i++) {
-    rows.push(Row(num_columns));
+    rows.push(<Row num_cells={num_columns}/>);
   }
   return <div>{rows}</div>
 }
 
-function App() {
-  return Grid(NUM_ROWS, NUM_COLS);
+
+
+// High level app component. has messy state.
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      num_rows: 3,
+      num_cols: 3
+    }
+    this.addRow = this.addRow.bind(this);
+  }
+
+  addRow() {
+    this.setState(prev_state => ({
+      num_rows: prev_state.num_rows + 1
+    }));
+  }
+
+  render() {
+    const b = (
+      <Button variant="raised"
+              color="primary"
+              onClick={this.addRow}>
+        add row 
+      </Button>
+    )
+
+    return (
+      <div>
+         <div>{b}</div>
+         <div>
+            <Grid num_rows={this.state.num_rows} 
+                  num_columns={this.state.num_cols}/>
+         </div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
