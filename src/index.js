e2589ebd4d050187f85da3a4951a339d9cc4ae10
omersagy <omersagyemails@gmail.com>
2018-04-19 23:35:07 -0400

    grid

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const NUM_ROWS = 3;
+const NUM_COLS = 10;
+
+function Cell() {
+  return <span>O</span>;
+}
+
+function Row(num_cells) {
+  let cells = [];
+  for (let i = 0; i < num_cells; i++) {
+    cells.push(Cell());
+  }
+  return <div>{cells}</div>
+}
+
+function Grid(num_rows, num_columns) {
+  let rows = [];
+  for (let i = 0; i < num_rows; i++) {
+    rows.push(Row(num_columns));
+  }
+  return <div>{rows}</div>
+}
+
 function App() {
-  return <p>What??</p>;
+  return Grid(NUM_ROWS, NUM_COLS);
 }
 
 ReactDOM.render(
